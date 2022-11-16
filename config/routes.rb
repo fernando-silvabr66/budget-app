38494ac26974452ebdd930ce44cf62164748d8fb
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-16 10:47:27 -0300

    Create controllers and views for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  get 'expenses/index'
+  get 'expenses/show'
+  get 'expenses/new'
+  get 'expenses/edit'
+  get 'expenses/delete'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/delete'
+  get 'entities/index'
+  get 'entities/show'
+  get 'entities/new'
+  get 'entities/edit'
+  get 'entities/delete'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
