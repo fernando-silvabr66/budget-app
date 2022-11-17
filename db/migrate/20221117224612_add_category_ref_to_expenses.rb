33b18d4b3f6baba81d7118c7234b1b4d6a4d89fb
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-17 20:00:32 -0300

    Fix migration files errors from tests

--- db/migrate/20221117224612_add_category_ref_to_expenses.rb
+++ db/migrate/20221117224612_add_category_ref_to_expenses.rb
@@ -1,5 +1,5 @@
 class AddCategoryRefToExpenses < ActiveRecord::Migration[7.0]
   def change
-    add_reference :settlements, :group, null: false, foreign_key: true
+    add_reference :expenses, :category, null: false, foreign_key: true
   end
 end
