c8fcec3615cf6812bb26655c8704a2193d1b7122
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-17 19:26:36 -0300

    Update categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,69 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
+
   
   def index
+    @categories = Category.all
   end
 
   def show
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def delete
   end
 
   def destroy
+    @category.destroy
+
+    respond_to do |format|
+      format.html { redirect_to categorys_url, notice: 'Category was successfully destroyed.' }
+      format.json { head :no_content }
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 
 end
