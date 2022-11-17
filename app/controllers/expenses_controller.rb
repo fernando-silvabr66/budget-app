a8105c054f923973ba4ed5de3b7ce9abc77dc3cf
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-17 19:26:55 -0300

    Update expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,27 +1,75 @@
 class ExpensesController < ApplicationController
+  before_action :set_category, only: %i[new create edit update destroy]
+  before_action :set_expense, only: %i[show edit update destroy]
   
   def index
+    @expenses = Expense.all
   end
 
   def show
   end
 
   def new
+    @expense = Expense.new
   end
 
   def create
+    @expense = Expense.new(expense_params)
+    @expense.category_id = @category.id
+    @expense.user_id = current_user.id
+
+    respond_to do |format|
+      if @expense.save
+        format.html { redirect_to @category, notice: 'Expense was successfully created.' }
+        format.json { render :show, status: :created, location: @expense }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @expense.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @expense.update(expense_params)
+        format.html { redirect_to @category, notice: 'Expense was successfully updated.' }
+        format.json { render :show, status: :ok, location: @expense }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @expense.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def delete
   end
 
   def destroy
+    @expense.destroy
+
+    respond_to do |format|
+      format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def expense_params
+    params.require(:expense).permit(:name, :amount).merge(user_id: current_user.id)
   end
 
 end
