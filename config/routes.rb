59b8b903e1bd74420b25ed14c067b1a67743dd73
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-18 20:20:54 -0300

    Review routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,11 @@
-Rails.application.routes.draw do  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+Rails.application.routes.draw do
   devise_for :users
   
-  root "splash#index"
-
-  resources :users
-  resources :categories do
-    resources :expenses
+  resources :users, only: %i[index show] do
+    resources :groups, only: %i[ new create  show ] do 
+      resources :categories, only: %i[ new create ]
+    end
   end
 
-  # get 'users/index'
-  # get 'users/show'
-  # get 'users/new'
-  # get 'users/edit'
-  # get 'users/update'
-  # get 'users/delete'
-  # get 'users/destroy'
-
-  # get 'categories/index'
-  # get 'categories/show'
-  # get 'categories/new'
-  # get 'categories/edit'
-  # get 'categories/update'
-  # get 'categories/delete'
-  # get 'categories/destroy'
-
-  # get 'expenses/index'
-  # get 'expenses/show'
-  # get 'expenses/new'
-  # get 'expenses/edit'
-  # get 'expenses/update'
-  # get 'expenses/delete'
-  # get 'expenses/destroy'
-
+  root 'splash#index'
 end
