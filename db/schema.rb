9690abfb7294ca490be851de62fc4be417e5ce6e
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-18 20:20:35 -0300

    Review all migrations - complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_224612) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_192644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.text "name"
-    t.text "icon"
+    t.string "name"
+    t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table "expenses", force: :cascade do |t|
+  create_table "category_groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["category_id"], name: "index_category_groups_on_category_id"
+    t.index ["group_id"], name: "index_category_groups_on_group_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
     t.string "name"
-    t.integer "amount"
+    t.text "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "category_id", null: false
-    t.index ["category_id"], name: "index_expenses_on_category_id"
-    t.index ["user_id"], name: "index_expenses_on_user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,11 +50,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_224612) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "expenses", "categories"
-  add_foreign_key "expenses", "users"
+  add_foreign_key "category_groups", "categories"
+  add_foreign_key "category_groups", "groups"
+  add_foreign_key "groups", "users"
 end
