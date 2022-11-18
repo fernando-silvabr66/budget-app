9713f21f4a5677b4d207b7ca4812d68c8dc36ad6
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-18 09:29:00 -0300

    Continue fixing devise errors

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,26 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show edit update destroy]
 
+  # GET /categories or /categories.json
   def index
     @categories = Category.all
   end
 
-  def show
-  end
+  # GET /categories/1 or /categories/1.json
+  def show; end
 
+  # GET /categories/new
   def new
     @category = Category.new
   end
 
+  # GET /categories/1/edit
+  def edit; end
+
+  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
     @category.user_id = current_user.id
-
     respond_to do |f|
       if @category.save
         f.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
@@ -27,9 +32,7 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def edit
-  end
-
+  # PATCH/PUT /categories/1 or /categories/1.json
   def update
     respond_to do |f|
       if @category.update(category_params)
@@ -42,15 +45,14 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def delete
-  end
-
+  # DELETE /categories/1 or /categories/1.json
   def destroy
     @category.destroy
 
     respond_to do |f|
-      f.html { redirect_to categorys_url, notice: 'Category was successfully destroyed.' }
+      f.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
       f.json { head :no_content }
+    end
   end
 
   private
@@ -64,5 +66,4 @@ class CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(:name, :icon)
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,3 @@
-# class UsersController < ApplicationController
-#   before_action :set_user, only: %i[show edit update destroy]
-
-#   def index
-#     @user = current_user
-#   end
-
-#   def show
-#   end
-
-#   def new
-#   end
-
-#   def create
-#   end
-
-#   def edit
-#   end
-
-#   def update
-#   end
-
-#   def delete
-#   end
-
-#   def destroy
-#   end
-
-# end
-
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
